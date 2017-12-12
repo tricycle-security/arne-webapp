74291c5c6ebdcc2b44539b2dbc7863cab3676d25
Hans <hans3132@live.nl>
2017-12-12 09:46:20 +0100

    able to edit alerts

--- controller/alertController.js
+++ controller/alertController.js
@@ -1,34 +1,38 @@
 app.controller('alertController', function ($routeParams, $scope, $timeout)
 {
+    
+    this.editableAlert = {
+    }
+    
     console.log('alertController')
     this.title = "Alerts!"
-    
+
     this.alerts = [
     ]
-    
 
-    
+
+
     var config = {
-    apiKey: "AIzaSyB9vsnSG_L4Cc_io4Z0cGE8_rhR66QBEVk",
-    authDomain: "tricycle-41751.firebaseapp.com",
-    databaseURL: "https://tricycle-41751.firebaseio.com",
-    projectId: "tricycle-41751",
-    storageBucket: "tricycle-41751.appspot.com",
-    messagingSenderId: "284978501316"
+        apiKey: "AIzaSyB9vsnSG_L4Cc_io4Z0cGE8_rhR66QBEVk",
+        authDomain: "tricycle-41751.firebaseapp.com",
+        databaseURL: "https://tricycle-41751.firebaseio.com",
+        projectId: "tricycle-41751",
+        storageBucket: "tricycle-41751.appspot.com",
+        messagingSenderId: "284978501316"
     };
 //    firebase.initializeApp(config);
-    
-    
+
+
 //
 //    var email = "0889529@hr.nl";
 //    var pass = "toor12";
 
     var database = null
     // Initialize the firebase
-    try{
+    try {
         firebase.initializeApp(config);
     }
-    catch(Exception)
+    catch (Exception)
     {
         console.log('firebase already exists');
         console.log(Exception);
@@ -36,11 +40,11 @@ app.controller('alertController', function ($routeParams, $scope, $timeout)
     finally {
         database = firebase.database();
     }
-    
+
     var email = "contact@tricycle-sec.nl"
-    var pass = "nope"
-    
-    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+    var pass = "nope!"
+
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -49,79 +53,97 @@ app.controller('alertController', function ($routeParams, $scope, $timeout)
         console.log(errorCode);
         console.log(errorMessage);
     });
-    
+
     var self = this
-    function retrieveAlerts(){
+    function retrieveAlerts() {
         var alertRef = database.ref().child('alerts');
-        alertRef.on('child_added', function(snap){
-        
+        alertRef.on('child_added', function (snap) {
+
             var tempAlert = snap.val();
             console.log(tempAlert)
 //            var tempAlertObject = {active: tempAlert.active, description: tempAlert.description, id: tempAlert.id, kind: tempAlert.kind, location: tempAlert.location, time: tempAlert.time};
 
             self.alerts.push(snap.val()); //allusers contains all userinfo + onLocation
 
-            $timeout(function (){
+            $timeout(function () {
                 $scope.$apply();
             });
-        
+
         });
-        alertRef.on('child_changed', function(snap){
+        alertRef.on('child_changed', function (snap) {
             console.log(snap.val())
             for (var i = 0; i < self.alerts.length; i++) {
-                if(self.alerts[i].id === snap.val().id){
+                if (self.alerts[i].id === snap.val().id) {
                     self.alerts[i] = snap.val()
-                    $timeout(function (){
+                    $timeout(function () {
                         $scope.$apply();
                     });
                     break;
                 }
             }
-        
+
         });
-        alertRef.on('child_removed', function(snap){
+        alertRef.on('child_removed', function (snap) {
             console.log(snap.val())
             for (var i = 0; i < self.alerts.length; i++) {
-                if(self.alerts[i].id === snap.val().id){
+                if (self.alerts[i].id === snap.val().id) {
                     self.alerts.splice(i, 1);
-                    $timeout(function (){
+                    $timeout(function () {
                         $scope.$apply();
                     });
                     break;
                 }
             }
-        
+
         });
     }
     
-    
-    this.sendAlert = function(alert) {
-        console.log('sendalert')
-        
-        if(alert != null){
+    this.createAlert = function (alert) {
+        console.log('createAlert')
+        console.log(self.editableAlert)
+        if (alert != null) {
             console.log(alert)
             alert.active = true
             var id = firebase.database().ref().child('alerts').push().key;
             alert.id = id;
             alert.time = (new Date).getTime();
+            self.sendAlert(alert)
+        }
+    }
+
+
+    this.sendAlert = function (alert) {
+        console.log('sendalert')
+        if (alert != null) {
             firebase.database().ref('alerts/' + id).set(alert);
         }
+
+    }
+    this.selectAlert = function (alert) {
+        console.log("edit : ", alert)
+        self.editableAlert = alert
         
-}
-    
+    }
+    this.editAlert = function (alert) {
+        
+        self.editableAlert = alert
+        
+    }
+
+
 
     var notRunning = true
 
-    firebase.auth().onAuthStateChanged(function(user) {
-    if (user && notRunning) {
-        notRunning = false;
-        console.log('logged in!')
-        retrieveAlerts();
-        
-    } else {
-        console.log('not logged in!')
-    }
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user && notRunning) {
+            notRunning = false;
+            console.log('logged in!')
+            retrieveAlerts();
+
+        } else {
+            console.log('not logged in!')
+        }
     });
-    
-    
+
+
 });
--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,105 +1,56 @@
 app.controller('dashboardController', function ($routeParams, $scope, $timeout)
 {
     //test information
-    var testUser = {fname: "Test", lname: "Testing", uuid: 1, onLocation: true}
-    var testUser2 = {fname: "Test", lname: "Testing", uuid: 1, onLocation: false}
     this.allUsers = [];
-    this.allUsers.push(testUser);this.allUsers.push(testUser2);
+//    this.allUsers.push(testUser);this.allUsers.push(testUser2);
 
-    this.title = "Available HBVs";
+    this.title = "Available BHV";
 
-    /*var config = {
-        apiKey: "AIzaSyD8suoQjYrq58S3yHoA7HoXqIVy3HJiJYM",
-        authDomain: "festivalapp-9e0ae.firebaseapp.com",
-        databaseURL: "https://festivalapp-9e0ae.firebaseio.com",
-        storageBucket: "festivalapp-9e0ae.appspot.com",
-        messagingSenderId: "423570948804"
+    var config = {
+        apiKey: "AIzaSyB9vsnSG_L4Cc_io4Z0cGE8_rhR66QBEVk",
+        authDomain: "tricycle-41751.firebaseapp.com",
+        databaseURL: "https://tricycle-41751.firebaseio.com",
+        projectId: "tricycle-41751",
+        storageBucket: "tricycle-41751.appspot.com",
+        messagingSenderId: "284978501316"
     };
 
-    try{
+    var database = null
+    // Initialize the firebase
+    try {
         firebase.initializeApp(config);
     }
-    catch(Exception)
+    catch (Exception)
     {
         console.log('firebase already exists');
         console.log(Exception);
     }
     finally {
-        firebaseSet = true;
-    }*/
+        database = firebase.database();
+    }
 
-    //Jordi's Firebase
-    var config = {
-        apiKey: "AIzaSyByv741uQXo8RsFtML7ASYpYvMeYmOadfg",
-        authDomain: "ye-olde-project.firebaseapp.com",
-        databaseURL: "https://ye-olde-project.firebaseio.com",
-        projectId: "ye-olde-project",
-        storageBucket: "ye-olde-project.appspot.com",
-        messagingSenderId: "1006813715006"
-    };
+    var email = "contact@tricycle-sec.nl"
+    var pass = "nope"
 
-    var email = "0889529@hr.nl";
-    var pass = "toor12";
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log("authError Here:");
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+
 
     console.log("API: Jordi's firebase");
 
-    // Initialize the firebase
-//    var database = null;
-//    try{
-//        firebase.initializeApp(config);
-//    }
-//    catch(Exception)
-//    {
-//        console.log('firebase already exists');
-//        console.log(Exception);
-//    }
-//    finally {
-//        database = firebase.database();
-////        firebaseSet = true;
-//    }
-
-//    //Sign in
-//    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
-//        // Handle Errors here.
-//        var errorCode = error.code;
-//        var errorMessage = error.message;
-//
-//        console.log("authError Here:");
-//        console.log(errorCode);
-//        console.log(errorMessage);
-//    });
-    
-    //create basic tables and info
-    var uuid = 2;
-
-    database.ref("/Userinfo/" + "usergeninfo/" + uuid).set({
-    fname: 'Arne',
-    lname: 'Lname',
-    uuid: uuid
-    });
-
-    database.ref("/Userinfo/" + "userstatus/" +  uuid).set({
-    admin: 'active',
-    bhver: true,
-    checkinpole: false
-    });
-
-    database.ref("/Cardinfo/" + "cardID").set({
-    status: 'active',
-    uuid: uuid
-    });
-
-    database.ref("/CurrentStatus/" + uuid).set({
-    onLocation: true,
-    uuid: uuid
-    });
-
     var self = this;
-    var statusinfo = database.ref().child('CurrentStatus');
-    var userinfo = database.ref().child('Userinfo/usergeninfo');
+    var statusinfo = database.ref().child('currentstatus');
+    var userinfo = database.ref().child('userinfo/usergeninfo');
 
     //Gets users with their onLocation status
-    statusinfo.on('child_added', function(snap){
+    statusinfo.on('child_added', function (snap) {
         var tempSnap = snap.val();
         userinfo.child(snap.val().uuid).on('value', user => {
             var tempUserInfo = user.val();
@@ -108,59 +59,9 @@ app.controller('dashboardController', function ($routeParams, $scope, $timeout)
             self.allUsers.push(tempUser); //allusers contains all userinfo + onLocation
             console.log(self.allUsers);
 
-            $timeout(function (){
-                $scope.$apply();
-            });
+//            $timeout(function () {
+//                $scope.$apply();
+//            });
         });
     });
-});
-
-
-
-
-//helper
-//get all users
-    /*database.ref('Userinfo/usergeninfo').on("child_added", function (data){
-        var tempData = data.val();
-        var tempUser = {fname: tempData.fname, lname: tempData.lname, uuid: tempData.uuid};
-
-        console.log(tempUser);
-        self.allUsers.push(tempUser);
-        
-        $timeout(function (){
-            $scope.$apply();
-        });
-    });*/
-
-
-
-    /*database.ref('CurrentStatus').on("child_added", function (data){
-        var tempData = data.val();
-        var tempStatus = {onLocation: tempData.onLocation, uuid: tempData.uuid};
-
-        self.availabilityUsers.push(tempStatus);
-        console.log(self.availabilityUsers);
-
-        $timeout(function (){
-            $scope.$apply();
-        });
-
-    });*/
-
-    /*
-        var tempUuid = data.val().uuid;
-var tempAvailability = []; 
-database.ref('CurrentStatus').child('uuid').child(tempUuid).once('value', function(mediaSnap) {
-    console.log(tempUuid + ":");// + mediaSnap.val().onLocation);
-    console.log(mediaSnap.val());
-});*/
-/*
-database.ref('CurrentStatus/' + tempUuid).once('value', function(mediaSnap) {
-    console.log(tempUuid + ":");// + mediaSnap.val().onLocation);
-    console.log(mediaSnap.val());
-    tempAvailability = mediaSnap.val();
-});*/
-
-        //tempUser += tempAvailability;
-
-//console.log("here "+tempUser);
+ });
