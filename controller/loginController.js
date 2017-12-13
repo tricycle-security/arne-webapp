34494ad9f3ad90cb8e303aca0b2689c612dd6d68
JLU_0889529 <jordi.luijk@live.nl>
2017-12-13 14:46:27 +0100

    logout added

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,11 +1,18 @@
 app.controller('loginController', function ($scope, $rootScope, $window)
 {    
     this.title = "Log in";
-    
     $scope.submit = function (){
         console.log("logging in...");
-        var email = "contact@tricycle-sec.nl";
-        var password = "Test123!";
+
+        //submitted values
+        var email = $scope.email;
+        var password = $scope.password;
+
+        //Submitted value must not be null, null values prevents code from executing onAuthStateChanged()
+        if (email == null || password == null) {
+            email = "email";
+            password = "password";
+        }
 
         this.signIn = firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
             console.log("auth sign in Error Here:");
@@ -13,21 +20,17 @@ app.controller('loginController', function ($scope, $rootScope, $window)
             console.log(error.message);
         });
 
+        var user = firebase.auth().currentUser;
+
         firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
+                console.log("Logged in");
                 //redirect after login
                 $window.location.href = './#/';
             } else {
                 console.log('not logged in!')
+                this.error = "Error: not logged in!";
             }
         });
-    }
-
-    var user = firebase.auth().currentUser;
-    
-    if (user) {// User is signed in.
-        console.log("Logged in");
-    } else {// No user is signed in.
-        console.log("Logged out");
-    }
+    }  
 });
---
+++ views/logout/logout.js
@@ -0,0 +1,7 @@
+app.directive('logout', function ()
+{
+        return {
+            restrict: 'E',
+            templateUrl: "views/logout/logout.html",
+        };
+});
